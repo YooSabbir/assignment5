16e0b65ba42f3303a4a5a1a8168edf82edfd4b42
ahsabbir476-cloud <ahsabbir476@gmail.com>
2026-03-08 15:34:39 +0600

    modal added with dynamic punctionality

--- script/index.js
+++ script/index.js
@@ -5,9 +5,84 @@ const loadIssues = () => {
         .then((res) => res.json())
         .then((json) => {
             allIssues = json.data;
+            count.innerText = allIssues.length;
             displayIssues(json.data)
         })
 }
+
+const loadIssuedetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displaySingleIssue(details.data);
+}
+const displaySingleIssue = (single) => {
+
+    const modalContainer = document.getElementById("modal-container");
+
+    const statusColor =
+        single.status === "open" ? "badge-success" : "badge-secondary";
+
+    const priorityColor =
+        single.priority === "high" ? "badge-error" : single.priority === "medium" ? "badge-warning" : "badge-neutral";
+
+    modalContainer.innerHTML = 
+        `
+    
+            <!-- Title -->
+            <h2 class="text-2xl font-bold mb-3">
+            ${single.title}
+            </h2>
+
+            <!-- Status Row -->
+            <div class="flex items-center gap-3 text-sm mb-4">
+            <span class="badge ${statusColor} text-white">
+            ${single.status.toUpperCase()}
+            </span>
+
+            <span class="text-gray-500">
+            Opened by ${single.author} • ${new Date(single.createdAt).toLocaleDateString()}
+            </span>
+            </div>
+
+            <!-- Tags -->
+            <div class="flex gap-2 mb-4">
+            ${single.labels.map(label => `
+            <span class="badge badge-outline">
+            ${label.toUpperCase()}
+            </span>
+        `   
+    ).join("")}
+    </div>
+
+    <!-- Description -->
+    <p class="text-gray-600 mb-6">
+        ${single.description}
+    </p>
+
+    <!-- Info Box -->
+    <div class="bg-gray-100 rounded-lg p-5 flex justify-between">
+
+        <div>
+            <p class="text-sm text-gray-500">Assignee:</p>
+            <p class="font-semibold">
+                ${single.assignee ? single.assignee : "Not Assigned"}
+            </p>
+        </div>
+
+        <div>
+            <p class="text-sm text-gray-500">Priority:</p>
+            <span class="badge ${priorityColor} text-white">
+                ${single.priority.toUpperCase()}
+            </span>
+        </div>
+
+    </div>
+    `;
+
+    document.getElementById("my_modal_5").showModal();
+}
+
 
 const displayIssues = (issues) => {
     /*1.get the content and empty
@@ -19,22 +94,23 @@ const displayIssues = (issues) => {
     cardContainer.innerHTML = ''
     for (let issue of issues) {
         const card = document.createElement("div");
-        card.classList = "bg-[#f8fafc] border-t-5 border-green-500 rounded-lg shadow-sm p-4 space-y-3"
 
-        if (issue.status === "open") {
-            card.classList.add('border-green-500')
-        } else {
-            card.classList.add("border-purple-500")
-        }
+        //conditions
+        let borderColor = issue.status === "open"
+            ? "border-green-500"
+            : "border-purple-500";
+        let img = issue.status === "open" ? "assets/Open-Status.png" : "assets/Closed- Status .png";
+        let btnColor = issue.priority === "high" ? "bg-[#FEE2E2] text-[#EF4444]" : issue.priority === "medium" ? "bg-[#FEF3C7] text-[#D97706]" : issue.priority === "low" ? "bg-[#EEEFF2] text-[#9CA3AF]" : "bg-gray-100 text-gray-500";
 
-
-        card.innerHTML = `
+        card.innerHTML =
+        `
+            <div onclick="loadIssuedetail(${issue.id})" class="bg-[#f8fafc] border-t-5 ${borderColor} rounded-lg shadow-sm p-4 space-y-3">
                     <div class="flex justify-between items-center">
                         <div>
-                        <img class="h-6" src="assets/Open-Status.png" alt="">
+                        <img class="h-6" src="${img}" alt="">
 
                         </div>
-                        <span class="bg-[#FEE2E2] text-[#EF4444] text-xs font-semibold px-3 py-1 rounded-full">${issue.priority}</span>
+                        <span class="${btnColor} text-xs font-semibold px-3 py-1 rounded-full">${issue.priority}</span>
                     </div>
 
                     <h3 class="font-semibold text-[15px]">${issue.title}</h3>
@@ -52,19 +128,17 @@ const displayIssues = (issues) => {
                         <p>#${issue.id} by ${issue.author}</p>
                         <p>${issue.updatedAt}</p>
                     </div>
-   
-    `
+            </div>
+        `
         cardContainer.append(card)
     }
 }
-
 
 let btnAll = document.getElementById("btn-all")
 let btnOpened = document.getElementById("btn-open")
 let btnClosed = document.getElementById("btn-closed")
 let count = document.getElementById("count")
-
-
+// btn toggle effect ....
 document.getElementById("btn-all").addEventListener("click", function () {
     displayIssues(allIssues)
     count.innerText = allIssues.length;
@@ -76,7 +150,6 @@ document.getElementById("btn-all").addEventListener("click", function () {
     btnAll.classList.add('btn-primary')
 
 })
-
 document.getElementById("btn-open").addEventListener("click", function () {
     const openIssues = allIssues.filter(issue => issue.status === "open")
     count.innerText = openIssues.length;
@@ -100,19 +173,9 @@ document.getElementById("btn-closed").addEventListener("click", function () {
     btnClosed.classList.add('btn-primary')
 })
 
-
-
-function toggleEffect() {
-
-
-
-
-}
-
-
 loadIssues()
 
-
+// search ''''
 document.getElementById("searchBtn").addEventListener("click", function () {
 
     const searchInput = document.getElementById("searchInput");
